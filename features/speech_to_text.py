afa937735d7cd8a6307f9b5169ba6c9da6eb2718
Aviral Garg <contact@aviralgarg.com>
2019-11-03 19:17:16 -0800

    enhancement "improved text to speech calls"

--- features/speech_to_text.py
+++ features/speech_to_text.py
@@ -3,15 +3,18 @@ import speech_recognition as sr
 WAIT_UNTIL_BOOL_ANSWER = None
 
 
-def get_text_from_speech(_ptl=3):
+def get_text_from_speech(_timeout=0.5):
     text = ''
     r = sr.Recognizer()
     with sr.Microphone() as audio_source:
-        audio = r.listen(audio_source, phrase_time_limit=_ptl)
 
         try:
+            audio = r.listen(audio_source, timeout=_timeout)
             text = r.recognize_google(audio)
             print(f'Avi : {text}')
+        except sr.WaitTimeoutError:
+            # print('DEBUG: speechrecognition.WaitTimeoutError')
+            pass
         except sr.UnknownValueError:
             print('DEBUG: speechrecognition.UnknownValueError')
             pass
@@ -24,6 +27,6 @@ def str_to_bool_char(_str):
 
 
 def get_voice_input(bool_answer=False):
-    _s = get_text_from_speech(_ptl=WAIT_UNTIL_BOOL_ANSWER)
+    _s = get_text_from_speech(_timeout=WAIT_UNTIL_BOOL_ANSWER)
     _s = str_to_bool_char(_s) if bool_answer else _s
     return _s
